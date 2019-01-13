4dff6a99f9afec5f0423377e6af74af53304030f
Todd Cope <xander4jesus@yahoo.com>
2019-01-13 12:15:35 -0600

    Bump up network version number since we will be introducing incompatibilities.

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 
 #define PP2_VERSION_TITLE "Paintball Party 2"
 #define PP2_VERSION_MAJOR "0"
-#define PP2_VERSION_MINOR "856"
+#define PP2_VERSION_MINOR "86"
 #define PP2_VERSION_STRING (PP2_VERSION_TITLE " v" PP2_VERSION_MAJOR "." PP2_VERSION_MINOR)
 #define PP2_VERSION_NETWORK (PP2_VERSION_MAJOR "." PP2_VERSION_MINOR) // version used for online server list only
 
