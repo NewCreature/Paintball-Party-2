1c7a2429cac466fd160499a4e7ad8aac09ab89bc
Todd Cope <todd@t3-i.com>
2020-09-19 17:26:25 -0500

    Fix errors due to missing includes.

--- src/file/database/level.c
+++ src/file/database/level.c
@@ -1,29 +1,27 @@
-#include <allegro5/allegro5.h>
-#include <allegro5/allegro_image.h>
+#include "../../t3f/t3f.h"
 #include "level.h"
 #include "../level.h"
-#include "../../legacy/level.h"
-#include "../../pp2.h"
+#include "../../interface/preview.h"
 
 PP2_LEVEL_DATABASE_EXTRA * pp2_level_database_create(PP2_FILE_DATABASE * dp, int entry, int flags)
 {
 	ALLEGRO_PATH * new_path = NULL;
 	PP2_LEVEL_DATABASE_EXTRA * ep;
 	char newfn[1024] = {0};
-	
+
 	ep = malloc(sizeof(PP2_LEVEL_DATABASE_EXTRA));
 	if(!ep)
 	{
 		return NULL;
 	}
-	
+
 	new_path = al_clone_path(t3f_data_path);
 	al_append_path_component(new_path, "preview_cache");
 	al_append_path_component(new_path, "levels");
 	sprintf(newfn, "%lu.p2p", dp->entry[entry]->checksum);
 	al_set_path_filename(new_path, newfn);
 	strcpy(ep->preview, al_path_cstr(new_path, '/'));
-	
+
 	/* if preview does not exist, create it */
 	if((flags & PP2_DATABASE_FLAG_REGENERATE) || !al_filename_exists(al_path_cstr(new_path, '/')))
 	{
