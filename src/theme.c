3ffa3c64e1af3a67722bf151393f3a3c17e1ec4f
Todd Cope <todd@t3-i.com>
2020-10-18 10:02:15 -0500

    Themes will only point the game to resource locations.

--- src/theme.c
+++ src/theme.c
@@ -19,10 +19,37 @@ PP2_THEME * pp2_load_theme(const char * fn)
 	val = al_get_config_value(tp->config, "Paintball Party 2 Theme", "bitmap_bg_tile");
 	if(val)
 	{
-		if(!t3f_load_resource((void **)&tp->bitmap[PP2_THEME_BITMAP_BG_TILE], T3F_RESOURCE_TYPE_BITMAP, val, 0, 0, 0))
-		{
-			goto fail;
-		}
+		tp->bitmap_bg_tile_fn = val;
+	}
+	val = al_get_config_value(tp->config, "Paintball Party 2 Theme", "bitmap_load");
+	if(val)
+	{
+		tp->bitmap_load_fn = val;
+	}
+	val = al_get_config_value(tp->config, "Paintball Party 2 Theme", "font_load_info");
+	if(val)
+	{
+		tp->font_load_info_fn = val;
+	}
+	val = al_get_config_value(tp->config, "Paintball Party 2 Theme", "font_menu_header");
+	if(val)
+	{
+		tp->font_menu_header_fn = val;
+	}
+	val = al_get_config_value(tp->config, "Paintball Party 2 Theme", "font_menu_item");
+	if(val)
+	{
+		tp->font_menu_item_fn = val;
+	}
+	val = al_get_config_value(tp->config, "Paintball Party 2 Theme", "font_hud");
+	if(val)
+	{
+		tp->font_hud_fn = val;
+	}
+	val = al_get_config_value(tp->config, "Paintball Party 2 Theme", "font_weapon_info");
+	if(val)
+	{
+		tp->font_weapon_info_fn = val;
 	}
 	tp->theme_music_fn = al_get_config_value(tp->config, "Paintball Party 2 Theme", "theme_music_fn");
 	tp->menu_music_fn = al_get_config_value(tp->config, "Paintball Party 2 Theme", "menu_music_fn");
--- src/theme.h
+++ src/theme.h
@@ -3,7 +3,7 @@
 
 #include "t3f/font.h"
 
-#define PP2_THEME_MAX_BITMAPS           8
+/*#define PP2_THEME_MAX_BITMAPS           8
 #define PP2_THEME_BITMAP_BG_TILE        0
 #define PP2_THEME_BITMAP_LOGO           1
 #define PP2_THEME_BITMAP_HUD_SPLAT_FEST 2
@@ -18,14 +18,21 @@
 #define PP2_THEME_FONT_WEAPON_INFO 4
 
 #define PP2_THEME_LOAD_SCREEN_TYPE_DEFAULT 0
-#define PP2_THEME_LOAD_SCREEN_TYPE_CLASSIC 1
+#define PP2_THEME_LOAD_SCREEN_TYPE_CLASSIC 1 */
 
 typedef struct
 {
 
 	ALLEGRO_CONFIG * config;
-	ALLEGRO_BITMAP * bitmap[PP2_THEME_MAX_BITMAPS];
-	T3F_FONT * font[PP2_THEME_MAX_FONTS];
+//	ALLEGRO_BITMAP * bitmap[PP2_THEME_MAX_BITMAPS];
+//	T3F_FONT * font[PP2_THEME_MAX_FONTS];
+	const char * bitmap_load_fn;
+	const char * bitmap_bg_tile_fn;
+	const char * font_load_info_fn;
+	const char * font_menu_header_fn;
+	const char * font_menu_item_fn;
+	const char * font_hud_fn;
+	const char * font_weapon_info_fn;
 	const char * theme_music_fn;
 	const char * menu_music_fn;
 
