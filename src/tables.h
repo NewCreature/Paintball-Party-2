ca9aaefabdcf01b70366f812bfd9c50e5d0b17b9
Todd Cope <todd@t3-i.com>
2020-10-21 14:40:41 -0500

    Move table to correct module.

--- src/tables.h
+++ src/tables.h
@@ -3,7 +3,6 @@
 
 #include "file/character.h"
 
-extern float pp2_angle_table[PP2_CHARACTER_MAX_STATES];
 extern int pp2_default_keys[4][9];
 extern char pp2_controller_binding_text[11][128];
 extern char * pp2_game_mode_text[16];
