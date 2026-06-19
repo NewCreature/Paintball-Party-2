063473712cd0dece6ce3aff0aa0f7f7bd62ecf26
Todd Cope <todd@t3-i.com>
2026-06-19 11:13:59 -0500

    Move 'text_entry' module to 'misc' folder.

--- src/text_entry.h
+++
@@ -1,9 +0,0 @@
-#ifndef PP2_TEXT_ENTRY_H
-#define PP2_TEXT_ENTRY_H
-
-void pp2_enter_text(const char * text, int mode);
-bool pp2_enter_text_logic(void);
-const char * pp2_get_entered_text(void);
-int pp2_get_text_entry_state(void);
-
-#endif
